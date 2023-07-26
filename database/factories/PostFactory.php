c8b4959fd56128263f9717747724d662c1090acd
luis colca <luis.7colca@gmail.com>
2023-07-25 14:31:38 -1200

    Mi Tercer commit de <Luiss F Colca Colca>

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,7 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+use App\Models\Post;
+use Illuminate\Support\Str;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
  */
@@ -14,10 +15,15 @@ class PostFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    protected $model = Post::class;
+
     public function definition(): array
     {
         return [
-            //
+            'user_id' => $this->faker->numberBetween(1, 20),
+            'title' => $this->faker->sentence,
+            'slug' => $this->faker->slug,
+            'content' => $this->faker->paragraphs(3, true), //ojo
         ];
     }
 }
